a995eb4a0f69ec6352b2069065d594438dbb7535
jeffm24 <jmariconda24@gmail.com>
2016-04-27 12:38:43 -0400

    Added positive/negative change-based highlighting to tickerItem and searchTickerItem

--- htmlBuilder.js
+++ htmlBuilder.js
@@ -14,72 +14,76 @@ function buildSearchTickerItem (info) {
         }
     }
 
-    var tickerDataHtml = '<div class="panel panel-default tickerItem">' +
-        '<a data-toggle="collapse" data-parent="#accordion" href="#collapse' + info.symbol + '">' +
-            '<div class="panel-heading">' +
-                '<h3 class="panel-title">' + info.symbol + ': $' + info.Ask + ' <span id="change-' + info.symbol + '">(' + info.ChangeinPercent + ')</span>' +
-                    '<button type="button" id="saveTickerBtn" class="btn btn-primary pull-right"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button><div class="clearfix"></div>' +
-                '</h3>' +
-            '</div>' +
-        '</a>' +
-        '<div id="collapse' + info.symbol + '" class="panel-collapse collapse in">' +
-            '<div class="panel-body">' +
-                '<div class="row">' +
-                    '<div class="col-xs-12 col-sm-6">' +
-                        '<div class="sub-panel graph-panel">' +
-                            '<div class="d3-wrapper" id="d3-' + info.symbol + '">' +
-                                '<img src="/assets/images/graph.png" />' +
-                            '</div>' +
+    var highlightClass;
+
+    if (info.ChangeinPercent.charAt(0) === '+') {
+        highlightClass = "positive";
+    } else {
+        highlightClass = "negative";
+    }
+
+    var tickerDataHtml = '<div class="panel panel-default tickerItem ' + highlightClass + '">' +
+        '<div class="panel-heading">' +
+            '<h3 class="panel-title">' + info.symbol + ' <span class="change-percent ' + highlightClass + '">(' + info.ChangeinPercent + ')</span>' +
+                '<button type="button" id="saveTickerBtn" class="btn btn-primary pull-right"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button><div class="clearfix"></div>' +
+            '</h3>' +
+        '</div>' +
+        '<div class="panel-body">' +
+            '<div class="row">' +
+                '<div class="col-xs-12 col-sm-6">' +
+                    '<div class="sub-panel graph-panel">' +
+                        '<div class="d3-wrapper" id="d3-' + info.symbol + '">' +
+                            '<img src="/assets/images/graph.png" />' +
                         '</div>' +
                     '</div>' +
-                    '<div class="col-xs-12 col-sm-6">' +
-                        '<div class="sub-panel stat-panel">' +
-                            '<h4>Statistics</h4>' +
-                            '<hr />' +
-                            '<div class="row">' +
-                                '<div class="col-xs-6">' +
-                                    '<div class="stat-item">' +
-                                        '<p class="stat-val">$' + info.Open + '</p>' +
-                                        '<p class="stat-name">Open</p>' +
-                                    '</div>' +
-                                    '<div class="stat-item">' +
-                                        '<p class="stat-val">$' + info.DaysHigh + '</p>' +
-                                        '<p class="stat-name">Today\'s High</p>' +
-                                    '</div>' +
-                                    '<div class="stat-item">' +
-                                        '<p class="stat-val">$' + info.DaysLow + '</p>' +
-                                        '<p class="stat-name">Today\'s Low</p>' +
-                                    '</div>' +
-                                    '<div class="stat-item">' +
-                                        '<p class="stat-val">$' + info.YearHigh + '</p>' +
-                                        '<p class="stat-name">52 Wk High</p>' +
-                                    '</div>' +
-                                    '<div class="stat-item">' +
-                                        '<p class="stat-val">$' + info.YearLow + '</p>' +
-                                        '<p class="stat-name">52 Wk Low</p>' +
-                                    '</div>' +
+                '</div>' +
+                '<div class="col-xs-12 col-sm-6">' +
+                    '<div class="sub-panel stat-panel">' +
+                        '<h4>Statistics</h4>' +
+                        '<hr />' +
+                        '<div class="row">' +
+                            '<div class="col-xs-6">' +
+                                '<div class="stat-item">' +
+                                    '<p class="stat-val">$' + info.Open + '</p>' +
+                                    '<p class="stat-name">Open</p>' +
                                 '</div>' +
-                                '<div class="col-xs-6">' +
-                                    '<div class="stat-item">' +
-                                        '<p class="stat-val">' + info.Volume + '</p>' +
-                                        '<p class="stat-name">Volume</p>' +
-                                    '</div>' +
-                                    '<div class="stat-item">' +
-                                        '<p class="stat-val">' + info.AverageDailyVolume + '</p>' +
-                                        '<p class="stat-name">Average Volume</p>' +
-                                    '</div>' +
-                                    '<div class="stat-item">' +
-                                        '<p class="stat-val">$' + info.MarketCapitalization + '</p>' +
-                                        '<p class="stat-name">Market Cap</p>' +
-                                    '</div>' +
-                                    '<div class="stat-item">' +
-                                        '<p class="stat-val">' + info.PERatio + '</p>' +
-                                        '<p class="stat-name">P/E Ratio</p>' +
-                                    '</div>' +
-                                    '<div class="stat-item">' +
-                                        '<p class="stat-val">' + info.DividendYield + '</p>' +
-                                        '<p class="stat-name">Div/Yield</p>' +
-                                    '</div>' +
+                                '<div class="stat-item">' +
+                                    '<p class="stat-val">$' + info.DaysHigh + '</p>' +
+                                    '<p class="stat-name">Today\'s High</p>' +
+                                '</div>' +
+                                '<div class="stat-item">' +
+                                    '<p class="stat-val">$' + info.DaysLow + '</p>' +
+                                    '<p class="stat-name">Today\'s Low</p>' +
+                                '</div>' +
+                                '<div class="stat-item">' +
+                                    '<p class="stat-val">$' + info.YearHigh + '</p>' +
+                                    '<p class="stat-name">52 Wk High</p>' +
+                                '</div>' +
+                                '<div class="stat-item">' +
+                                    '<p class="stat-val">$' + info.YearLow + '</p>' +
+                                    '<p class="stat-name">52 Wk Low</p>' +
+                                '</div>' +
+                            '</div>' +
+                            '<div class="col-xs-6">' +
+                                '<div class="stat-item">' +
+                                    '<p class="stat-val">' + info.Volume + '</p>' +
+                                    '<p class="stat-name">Volume</p>' +
+                                '</div>' +
+                                '<div class="stat-item">' +
+                                    '<p class="stat-val">' + info.AverageDailyVolume + '</p>' +
+                                    '<p class="stat-name">Average Volume</p>' +
+                                '</div>' +
+                                '<div class="stat-item">' +
+                                    '<p class="stat-val">$' + info.MarketCapitalization + '</p>' +
+                                    '<p class="stat-name">Market Cap</p>' +
+                                '</div>' +
+                                '<div class="stat-item">' +
+                                    '<p class="stat-val">' + info.PERatio + '</p>' +
+                                    '<p class="stat-name">P/E Ratio</p>' +
+                                '</div>' +
+                                '<div class="stat-item">' +
+                                    '<p class="stat-val">' + info.DividendYield + '</p>' +
+                                    '<p class="stat-name">Div/Yield</p>' +
                                 '</div>' +
                             '</div>' +
                         '</div>' +
